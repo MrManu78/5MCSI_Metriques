21de78d8eea6b5ece76cebb7d12bdfd37d68932f
MrManu78 <149469053+MrManu78@users.noreply.github.com>
2024-03-26 11:04:55 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -32,6 +32,9 @@ def mongraphique():
 @app.route("/histogramme/")
 def histogramme():
     return render_template("histogramme.html")
+@app.route("/commits/")
+def commits():
+    return render_template("commits.html")
   
 if __name__ == "__main__":
   app.run(debug=True)
